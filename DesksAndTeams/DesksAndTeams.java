e11711a5cd496bd1c1596085d2d5cf2da67c889e
mikel <emikelsolabarrieta@gmail.com>
2016-02-15 11:41:45 -0430

    DesksAndTeams.java exercise

---
+++ DesksAndTeams/DesksAndTeams.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+
+public class DesksAndTeams {
+	
+	public static void main(String[] args) {
+		System.out.println("*First test: " + solutionR(new int[] {2, 4, 1}, new int[] {3, 2, 2}) + "\n");
+		System.out.println("*Second test: " + solutionR(new int[] {2, 4}, new int[] {3, 2, 2}) + "\n");
+		System.out.println("*Third test: " + solutionR(new int[] {1}, new int[] {2}) + "\n");
+		System.out.println("*Fourth test: " + solutionR(new int[] {3, 4, 5}, new int[] {1}) + "\n");
+		System.out.println("*Fifth test: " + solutionR(new int[] {1}, new int[] {2, 3}) + "\n");
+	}
+
+	public static boolean solutionR(int[] desk, int[] team) {
+		System.out.print("Desk" + Arrays.toString(desk) + " ");
+		System.out.println("Team" + Arrays.toString(team) + " ");
+
+		if(team.length == 0) {
+			return true;
+		}
+		else if(desk.length == 0) {
+			return false;
+		}
+
+		while(true) {
+			int i = 0;
+
+			if(team[i] == desk[i]) {
+				desk = Arrays.copyOfRange(desk, i + 1, desk.length);
+				team = Arrays.copyOfRange(team, i + 1, team.length);
+			}
+			else if(desk[i] > team[i]) {
+				desk[i] = desk[i] - team[i];
+				team = Arrays.copyOfRange(team, i, team.length - 1);
+			}
+			else {
+				if(i + 1 < desk.length) {
+					desk[i] = desk[i] + 1;
+					desk[i + 1] = desk[i + 1] - 1;					
+
+					if(desk[i + 1] == 0) {
+						desk = Arrays.copyOfRange(desk, i, desk.length - 1);
+					}
+				}
+				else {
+					return false;
+				}
+			}
+
+			return solutionR(desk, team);
+		}
+	}
+}
